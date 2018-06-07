762d0f630e3b90c525104d7c75f6c2c71aa44c6a
Yami Odymel <yamiodymel@gmail.com>
2018-06-07 19:33:27 +0800

    200 改為 http.StatusOK

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/go-mego/bcrypt"
 	"github.com/go-mego/mego"
 )
@@ -8,7 +10,7 @@ import (
 func main() {
 	e := mego.Default()
 	e.GET("/", bcrypt.New(), func(c *mego.Context, cpt *bcrypt.Crypt) {
-		c.String(200, cpt.Hash("myPassword"))
+		c.String(http.StatusOK, cpt.Hash("myPassword"))
 	})
 	e.Run()
 }
